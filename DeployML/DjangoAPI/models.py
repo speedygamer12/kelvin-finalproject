cf6b80faca2a6b1ce482b9d0c971c5ae8cbaea3c
speedygamer12 <50147066+speedygamer12@users.noreply.github.com>
2021-10-28 00:13:18 +0100

    update model

--- DeployML/DjangoAPI/models.py
+++ DeployML/DjangoAPI/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Surveyor(models.Model):
-    STDE = models.FloatField('Shaft Drive-end Temperature Reading')
-    MTDE = models.FloatField('Motor Drive-end Temperature Reading')
-    SUDEM = models.IntegerField('Shaft drive-end maximum decibel reading')
-    SUDEC = models.IntegerField('Shaft drive-end carpet decibel reading')
-    MVDEH = models.FloatField('Motor drive-end vertical velocity')
-    MVDEV = models.FloatField('Motor drive-end horizontal velocity')
-    MVDEA = models.FloatField('Motor drive-end axial velocity')
-    SVDEH = models.FloatField('Shaft drive-end horizontal velocity')
-    SVDEV = models.FloatField('Shaft drive-end vertical velocity')
-    FD = models.IntegerField('feed capacity')
+    STDE = models.FloatField(' Shaft drive-end temperature reading (degrees Celsius)')
+    MTDE = models.FloatField('Motor drive-end temperature reading (degrees Celsius)')
+    SUDEM = models.IntegerField('Shaft drive-end maximum decibel reading (decibels)')
+    SUDEC = models.IntegerField('Shaft drive-end carpet decibel reading (decibels)')
+    MVDEH = models.FloatField('Motor drive-end vertical velocity (metre per second)')
+    MVDEV = models.FloatField('Motor drive-end horizontal velocity (metre per second)')
+    MVDEA = models.FloatField('Motor drive-end axial velocity (metre per second)')
+    SVDEH = models.FloatField('Shaft drive-end horizontal velocity (metre per second)')
+    SVDEV = models.FloatField('Shaft drive-end vertical velocity (metre per second)')
+    FD = models.IntegerField('feed capacity (kilogram)')
 
     def __str__(self):
         return self.STDE
