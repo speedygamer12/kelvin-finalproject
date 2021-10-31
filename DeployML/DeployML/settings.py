8f3179f456a8a0ae9771c76af99d5ce4175b7710
speedygamer12 <50147066+speedygamer12@users.noreply.github.com>
2021-10-31 12:01:42 +0100

    minor

--- DeployML/DeployML/settings.py
+++ DeployML/DeployML/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['kelvin-mlproject.herokuapp.com', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'formtools',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
